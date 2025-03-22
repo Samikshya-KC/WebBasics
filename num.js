40b42858faa1be04eb2fc9d80b2d5e3c398a355d
Samikshya-KC <samikshyakc021@gmail.com>
2025-03-22 16:03:15 +0545

    dynamically generate number

---
+++ num.js
@@ -0,0 +1,15 @@
+let titleH1 = document.getElementById("title");
+titleH1.style.color = "red";
+
+function printNumbers() {
+    let upto = document.getElementById("upperRange").value;
+    titleH1.innerText = "Numbers form 1-" + upto;
+
+    let numbersDiv = document.getElementById("numbers");
+    let numbersHtml = "";
+    for (let i = 1; i <= upto; i++) {
+        numbersHtml = numbersHtml + "<h2 class='badge text-bg-warning'> " + i + "</h2>";
+    }
+    numbersDiv.innerHTML = numbersHtml;
+
+}
