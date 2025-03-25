c7fb9ebcb1ce11ceec58241f29355577084c7f07
Samikshya-KC <samikshyakc021@gmail.com>
2025-03-25 14:01:38 +0545

    C# with included pre-built library

---
+++ BackendBasics/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+byte age=12;
+Console.WriteLine(age);
+//Allow user to enter their full name and print the name in uppercase
+Console.Write("Your Fullname:");
+string? fullname= Console.ReadLine();
+string nameInUpper = fullname.ToUpper();
+Console.WriteLine(nameInUpper);
+string firstFriend="Maria";
+string secondFriend="Scott";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
